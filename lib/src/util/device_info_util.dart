e950b6b8a15215af606a159cbfe00ec4443b8464
kagwicharles <kagwitheuri@gmail.com>
2023-12-11 10:36:44 +0300

    update: add emulator check

--- lib/src/util/device_info_util.dart
+++ lib/src/util/device_info_util.dart
@@ -22,6 +22,19 @@ class DeviceInfo {
         exit(0);
       });
     }
+    if (isEmulator()) {
+      Fluttertoast.showToast(
+          msg: "This app cannot work on an emulator!",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      Future.delayed(const Duration(seconds: 5), () {
+        exit(0);
+      });
+    }
   }
 
   static Future<bool> _checkDeviceRooted() async {
@@ -39,4 +52,17 @@ class DeviceInfo {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     return packageInfo.version;
   }
+
+  static bool isEmulator() {
+    if (Platform.isAndroid) {
+      return (Platform.environment['ANDROID_EMULATOR'] == '1') ||
+          (Platform.isAndroid && !isPhysicalDevice);
+    } else if (Platform.isIOS) {
+      return (Platform.environment['SIMULATOR_DEVICE_NAME'] != null) ||
+          (Platform.isIOS && !isPhysicalDevice);
+    }
+    return false;
+  }
+
+  static bool get isPhysicalDevice => !Platform.isAndroid && !Platform.isIOS;
 }
