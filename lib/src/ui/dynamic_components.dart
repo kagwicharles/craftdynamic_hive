74028cab25afbd1935a7a71c13908ad63e212252
kagwicharles <kagwitheuri@gmail.com>
2024-02-01 10:37:55 +0300

    hidden values

--- lib/src/ui/dynamic_components.dart
+++ lib/src/ui/dynamic_components.dart
@@ -345,7 +345,7 @@ class HiddenWidget implements IFormWidget {
       }
 
       return Visibility(
-        visible: true,
+        visible: false,
         child: TextFormField(
           validator: (value) {
             AppLogger.appLogD(
