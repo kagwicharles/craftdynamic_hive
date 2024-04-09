3d4be7d153612d08459fe8707cb8f93d871cffe6
kagwicharles <kagwitheuri@gmail.com>
2024-04-09 08:46:12 +0300

    update: ui/ux change

--- lib/src/ui/forms/confirmation_form.dart
+++ lib/src/ui/forms/confirmation_form.dart
@@ -12,72 +12,69 @@ class ConfirmationForm {
       enableDrag: true,
       context: context,
       builder: (BuildContext context) {
-        return Container(
-            padding:
-                const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 4),
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              opacity: .1,
-              image: AssetImage(
-                'assets/launcher.png',
-              ),
-            )),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Text(
-                      "Confirm Transaction",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const Spacer(),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(1);
-                      },
-                      child: const Row(
-                          children: [Icon(Icons.close), Text("Cancel")]),
-                    ),
-                  ],
+        return ListView(shrinkWrap: true, children: [
+          Container(
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, top: 12, bottom: 4),
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                opacity: .1,
+                image: AssetImage(
+                  'assets/launcher.png',
                 ),
-                ListView(
-                  shrinkWrap: true,
-                  children: [
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    Form(
-                        key: formKey,
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: formItems.length,
-                            padding: const EdgeInsets.only(left: 14, right: 14),
-                            itemBuilder: (context, index) {
-                              return BaseFormComponent(
-                                  formItem: formItems[index],
-                                  moduleItem: moduleItem,
-                                  formItems: formItems,
-                                  formKey: formKey,
-                                  child: IFormWidget(formItems[index],
-                                          jsonText: input)
-                                      .render());
-                            })),
-                    const SizedBox(
-                      height: 44,
-                    ),
-                    SizedBox(
-                        width: 300,
-                        child: WidgetFactory.buildButton(context, () {
-                          Navigator.of(context).pop(0);
-                        }, "Continue".toUpperCase())),
-                    const SizedBox(
-                      height: 44,
-                    )
-                  ],
-                )
-              ],
-            ));
+              )),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        "Confirm Transaction",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      const Spacer(),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(1);
+                        },
+                        child: const Row(
+                            children: [Icon(Icons.close), Text("Cancel")]),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Form(
+                      key: formKey,
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: formItems.length,
+                          padding: const EdgeInsets.only(left: 14, right: 14),
+                          itemBuilder: (context, index) {
+                            return BaseFormComponent(
+                                formItem: formItems[index],
+                                moduleItem: moduleItem,
+                                formItems: formItems,
+                                formKey: formKey,
+                                child: IFormWidget(formItems[index],
+                                        jsonText: input)
+                                    .render());
+                          })),
+                  const SizedBox(
+                    height: 44,
+                  ),
+                  SizedBox(
+                      width: 300,
+                      child: WidgetFactory.buildButton(context, () {
+                        Navigator.of(context).pop(0);
+                      }, "Continue".toUpperCase())),
+                  const SizedBox(
+                    height: 44,
+                  )
+                ],
+              ))
+        ]);
       },
     );
   }
